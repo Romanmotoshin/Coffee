043742af8861c1a01dc1c2d444f256de0e2c59d6
roman <roman.motoshin@yandex.ru>
2022-09-14 09:54:05 +0300

    init commit

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,13 @@
+import './app-header.scss';
+
+const Header = () => {
+    return (
+        <div className='main'>
+            <div className="container">
+                пошел на хуй
+            </div>
+        </div>
+    )
+}
+
+export default Header;
---
+++ src/components/app/app.js
@@ -0,0 +1,11 @@
+import Header from "../app-header/app-header";
+
+const App = () => {
+    return (
+        <div className = 'app'>
+            <Header/>
+        </div>
+    );
+}
+
+export default App;
